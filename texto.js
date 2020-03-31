15f5106e035f409e219855e85d21b76b8a7a1d3a
Carlos Dantas <carlosdantas485@gmail.com>
2020-03-31 15:48:44 -0300

    [ ESTUDOS ]

---
+++ texto.js
@@ -0,0 +1,11 @@
+var listElement = document.querySelector('#app ul');
+
+var inputElement = document.querySelector('#app input');
+
+var buttonElement = document.querySelector('#app button');
+
+var texto = [
+    'Fazer cafe',
+    'Estidar JavaScript',
+    'Acessar canal do youtube',
+];
